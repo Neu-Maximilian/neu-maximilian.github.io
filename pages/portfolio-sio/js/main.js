5692ae34fc9f312c4183ed729ab9019a32441d67
neu.maximilian <neu.maximilian@proton.me>
2024-02-23 08:52:53 +0100

    Refactor mobile menu functionality

--- pages/portfolio-sio/js/main.js
+++ pages/portfolio-sio/js/main.js
@@ -1,50 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function checkWindowSize() {
-    // On verifie si la largeur de la fenetre est inferieur ou egale à 1000px (mobile)
-    if (window.innerWidth <= 1000) {
-      // On cache l'ancien menu
-      const navLinks = document.querySelector(".nav-links");
-      navLinks.classList.add("hide");
-      navLinks.style.height = "0px";
-      // On créer un nouveau menu si il n'existe pas
-      let burgerMenu = document.querySelector(".burger-menu");
-      if (!burgerMenu) {
-        burgerMenu = document.createElement("button");
-        burgerMenu.classList.add("burger-menu");
-        burgerMenu.textContent = "☰";
-      }
-      // On ajoute un event listener sur le nouveau menu
+function checkWindowSize() {
+  // On verifie si la largeur de la fenetre est inferieur ou egale à 1000px (mobile)
+  if (window.innerWidth <= 1000) {
+    // On cache l'ancien menu
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.add("hide");
+    navLinks.style.height = "0px";
+    // On créer un nouveau menu si il n'existe pas
+    let burgerMenu = document.querySelector(".burger-menu");
+    if (!burgerMenu) {
+      burgerMenu = document.createElement("button");
+      burgerMenu.classList.add("burger-menu");
+      burgerMenu.textContent = "☰";
+    }
+    // On ajoute le nouveau menu au DOM
+    const nav = document.querySelector("nav");
+    nav.appendChild(burgerMenu);
+    // On ajoute un event listener sur le bouton
+    if (document.querySelector(".burger-menu")) {
       burgerMenu.addEventListener("click", function () {
         // On affiche ou cache le menu
         navLinks.classList.toggle("hide");
         // On change le contenu du bouton
         if (navLinks.classList.contains("hide")) {
           burgerMenu.textContent = "☰";
-          navLinks.style.height = "0vh"; // Set to collapsed height
+          navLinks.style.height = "0vh";
         } else {
           burgerMenu.textContent = "✖";
-          navLinks.style.height = "auto"; // Set to expanded height
+          navLinks.style.height = "auto";
         }
       });
-      // On ajoute le nouveau menu au DOM
-      const nav = document.querySelector("nav");
-      nav.appendChild(burgerMenu);
-    }
-    // Si la largeur de la fenetre est superieur à 1000px
-    else {
-      // On supprime le nouveau menu
-      const burgerMenu = document.querySelector(".burger-menu");
-      if (burgerMenu) {
-        burgerMenu.remove();
-        // On affiche l'ancien menu
-        const navLinks = document.querySelector(".nav-links");
-        navLinks.classList.remove("hide");
-      }
     }
   }
+  // Si la largeur de la fenetre est superieur à 1000px
+  else {
+    // On supprime le nouveau menu
+    const burgerMenu = document.querySelector(".burger-menu");
+    if (burgerMenu) {
+      burgerMenu.remove();
+      // On affiche l'ancien menu
+      const navLinks = document.querySelector(".nav-links");
+      navLinks.classList.remove("hide");
+    }
+  }
+}
 
-  window.addEventListener("load", checkWindowSize);
-  window.addEventListener("resize", checkWindowSize);
-  window.addEventListener("orientationchange", checkWindowSize);
-  checkWindowSize();
-});
+window.addEventListener("load", checkWindowSize);
+window.addEventListener("resize", checkWindowSize);
